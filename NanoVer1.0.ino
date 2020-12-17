f8f9ee3d087800ec5cdfc79a6e4b19de1ab84b52
Zshek Zeng <46489411+Zshek001@users.noreply.github.com>
2020-12-17 13:40:30 +0800

    Update NanoVer1.0.ino
    program architecture update

--- NanoVer1.0.ino
+++ NanoVer1.0.ino
@@ -7,13 +7,25 @@
 
 ---------------------------------------------------------------------------------------------*/
 
+#include <Adafruit_ILI9340.h>           // LED shield driver
+#include <SPI.h>                        // Board-to-shield communication
+#include <Wire.h>
+
+#include "MAX30105.h"
+#include "heartRate.h" //??what's this
+
+//pins NO. assignment
+#define CS 10
+#define DC 9
+#define RST 8
+
 //==========================================================================================//
 //								                1. Variable declaration  							                    //
 //==========================================================================================//
 
 // Master time control info //
-[......]			 		      // Loop frequency (Hz)
-[......];    		        // Loop period (us)
+int loopfreq = 100;			                              // Loop frequency (Hz)
+float loopperiod = 1000000 / (2 * loopfreq);    		  // Loop period (us)
 
 // Signal generation //
 [......]                // ON/OFF of signal generation
@@ -29,30 +41,17 @@
 
 
 //==========================================================================================//
-//					                  2. Main program (initialize environment)  			    	       	//
+//								                2. Function declaration  							                    //
 //==========================================================================================//
-void setup() {
 
-  // Turn on serial port (baud, bit per second) //
-  [......]
-
-  // Set analog read resolution (bit) //
-  [......]
-
+// serial controller
+void serialcontroller(){
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+  
 }
-
-//==========================================================================================//
-//							                   3. Main program (looping) 						                		//
-//==========================================================================================//
-void loop() {
-
-  // Set variable to record start time for loop time monitoring (i.e sampling period) //
-  [......]
-    
-  // Using counter to compute the phase of the waveform //
-  [......]
-    
-  //================================== Signal generation ===================================//
+// LED emission controller
+//================================== Signal generation ===================================//
 
   if [......] {   // ON/OFF of signal generation 
 
@@ -70,20 +69,52 @@ void loop() {
       [......]
 
     }
-
   }
 
-  //================================== Signal detection ===================================//
-  
+// data reader
+datareader(){
   if [......] {   // ON/OFF of signal detection
     
-    // Sampling at analog channel defined above //
-    [......] 
+  // Sampling at analog channel defined above //
+  [......] 
   
-    // Print the reading to serial port for visualization //  
-    [......] 
+  // Print the reading to serial port for visualization //  
+  [......] 
 
   }
+}
+
+// data processor
+
+// screen controller
+
+
+
+//==========================================================================================//
+//					                  3. Main program (initialize environment)  			    	       	//
+//==========================================================================================//
+// Turn on serial port (baud, bit per second) //
+void setup() {
+  serialcontroller();
+
+
+}
+
+//==========================================================================================//
+//							                   4. Main program (looping) 						                		//
+//==========================================================================================//
+void loop() {
+
+  // Set variable to record start time for loop time monitoring (i.e sampling period) //
+  [......]
+    
+  // Using counter to compute the phase of the waveform //
+  [......]
+    
+//================================== Signal generation ===================================//
+//================================== Signal detection ====================================//
+  
+
 
 
   // Set variable to record end time for loop time monitoring (i.e sampling period) //
@@ -93,3 +124,10 @@ void loop() {
   [......] 
 
 }
+
+//==========================================================================================//
+//								                6. Function realization  							                    //
+//==========================================================================================//
+
+
+
